9e9db5395dd4a3b86ad27852818c0b3fd5ec498b
Ihor Smyk <ismyk256@gmail.com>
2023-05-27 20:04:12 +0300

    upd

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../middlewares");
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+  },
+  {
+    versionKey: true,
+    timestamps: true,
+  }
+);
+
+userSchema.post("save", handleMongooseError);
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+};
